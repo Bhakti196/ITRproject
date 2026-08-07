e409ff819adf971e59482b5380d18ab1645fbce6
Bhakti196 <bhaktikhairanar2008@gmail.com>
2026-08-07 16:13:49 +0530

    new change

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from .views import RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
+
+    # Login
+    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+
+    # Refresh Access Token
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
